91fdcac91852b17949b8a070cbfb75787dd307c3
dev-junehee <dev.junehee@gmail.com>
2024-07-24 01:20:14 +0900

    Refactor: ValidationManager 구현 - 기존 유효성 검사 함수 이동

--- mood-folio/Presentation/Profile/ViewModel/ProfileViewModel.swift
+++ mood-folio/Presentation/Profile/ViewModel/ProfileViewModel.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-enum NicknameValidationError: Error {
-    case empty
-    case hasSpecialChar
-    case hasNumber
-    case invalidLength
-    case same
-}
-
-enum MBTIValidation {
-    
-}
-
 final class ProfileViewModel {
     
     // input
@@ -70,7 +58,7 @@ final class ProfileViewModel {
         guard let nickname = inputNicknameTextField.value else { return }
 
         do {
-            let result = try getNicknameValidationResult(nickname)
+            let result = try ValidationManager.shared.getNicknameValidation(nickname)
             if result {
                 outputNicknameIsValid.value = true
                 outputNicknameInvalidMessage.value = .success
@@ -94,40 +82,6 @@ final class ProfileViewModel {
         }
     }
     
-    // 닉네임 유효성 검사
-    private func getNicknameValidationResult(_ nickname: String) throws -> Bool {
-        let specialChars = ["@", "#", "$", "%"]
-        let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        
-        var hasSpecialChar = false
-        var hasNumber = false
-        
-        for char in specialChars {
-            for num in numbers {
-                if nickname.contains(char) {
-                    hasSpecialChar = true
-                }
-                if nickname.contains(num) {
-                    hasNumber = true
-                }
-            }
-        }
-        
-        if nickname.isEmpty || nickname.trimmingCharacters(in: .whitespaces).isEmpty {
-            throw NicknameValidationError.empty
-        } else if hasSpecialChar {
-            throw NicknameValidationError.hasSpecialChar
-        } else if hasNumber {
-            throw NicknameValidationError.hasNumber
-        } else if nickname.count < 2 || nickname.count >= 10 {
-            throw NicknameValidationError.invalidLength
-        } else if nickname == UserDefaultsManager.shared.nickname {
-            throw NicknameValidationError.same
-        } else {
-            return true
-        }
-    }
-    
     // MBTI 유효성 검사
     private func mbtiValidation() {
         guard let mbtiChar = inputMBTIButton.value else { return }
@@ -138,6 +92,5 @@ final class ProfileViewModel {
     private func saveUserAccount() {
         // UserDefaults에 유저 데이터 저장
     }
-    
     
 }
