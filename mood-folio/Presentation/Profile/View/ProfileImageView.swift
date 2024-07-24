df4b48adc51906862ca53756069af85a8a031916
dev-junehee <dev.junehee@gmail.com>
2024-07-24 14:34:10 +0900

    Refactor: ProfileImageView UI 구성 클로저로 변경

--- mood-folio/Presentation/Profile/View/ProfileImageView.swift
+++ mood-folio/Presentation/Profile/View/ProfileImageView.swift
@@ -11,10 +11,32 @@ import SnapKit
 final class ProfileImageView: BaseView {
     
     private let profileImageView = UIView()
-    let profileImage = UIImageView()
     
-    private let cameraImageView = UIView()
-    private let cameraImage = UIImageView()
+    let profileImage = {
+        let view = UIImageView()
+        view.backgroundColor = Resource.Color.white
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 50
+        view.layer.borderColor = Resource.Color.primary.cgColor
+        view.layer.borderWidth = Constants.Integer.borderWidth
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let cameraImageView = {
+        let view = UIView()
+        view.backgroundColor = Resource.Color.primary
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private let cameraImage = {
+        let view = UIImageView()
+        view.image = Resource.SystemImage.camera
+        view.contentMode = .scaleAspectFit
+        view.tintColor = Resource.Color.white
+        return view
+    }()
     
     // 프로필 이미지 컬렉션 뷰
     lazy var profileCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
@@ -77,22 +99,6 @@ final class ProfileImageView: BaseView {
             $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(16)
             $0.bottom.equalTo(self.safeAreaLayoutGuide)
         }
-    }
-    
-    override func configureUI() {
-        profileImage.backgroundColor = Resource.Color.white
-        profileImage.clipsToBounds = true
-        profileImage.layer.cornerRadius = 50  // 추후 상수화 하기
-        profileImage.layer.borderColor = Resource.Color.primary.cgColor
-        profileImage.layer.borderWidth = Constants.Integer.borderWidth
-        profileImage.contentMode = .scaleAspectFit
-        
-        cameraImageView.backgroundColor = Resource.Color.primary
-        cameraImageView.layer.cornerRadius = 12  // 추후 상수화 하기
-        
-        cameraImage.image = Resource.SystemImage.camera
-        cameraImage.contentMode = .scaleAspectFit
-        cameraImage.tintColor = Resource.Color.white
     }
     
 }
---
+++ mood-folio/Presentation/Topic/TopicView.swift
@@ -0,0 +1,17 @@
+//
+//  TopicView.swift
+//  mood-folio
+//
+//  Created by junehee on 7/24/24.
+//
+
+import UIKit
+
+final class TopicView: BaseView {
+    
+    private let profile = {
+        
+        
+    }()
+    
+}
